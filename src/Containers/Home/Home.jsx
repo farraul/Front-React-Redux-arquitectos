39a63af3129704acb65de4986508dc3bfdeb461e
raul farl <raulfarllobell@gmail.com>
2021-12-18 00:08:15 +0100

    modicate the fils app js, and move the file app.scss

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+
+
+
+    return (
+
+        <p>hola</p>
+       )
+
+};
+
+
+export default Home;
