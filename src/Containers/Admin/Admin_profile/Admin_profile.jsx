ad1a75bfaf3bac031cefecf2e653de249c104834
raul farl <raulfarllobell@gmail.com>
2021-12-21 02:00:57 +0100

    admin update, install the boton delete account

--- src/Containers/Admin/Admin_profile/Admin_profile.jsx
+++ src/Containers/Admin/Admin_profile/Admin_profile.jsx
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Admin_menu_comp from '../../../Components/Admin_menu_comp/Admin_menu_comp';
 
-
-
-
-
-
-
 const Admin_profile = () => {
 
     //Hooks
-
 
     const see_update = async () => {
         let element = document.getElementById("my-update");
@@ -26,9 +19,20 @@ const Admin_profile = () => {
         let element_back = document.getElementById("open-files-background");
         element_back.classList.remove("see-update");
     }
+    const see_delete = async () => {
+        let element = document.getElementById("my-delete");
+        element.classList.add("my-style");
+        let element_back = document.getElementById("open-files-background-delete");
+        element_back.classList.add("see-update");
+        console.log("run: ")
+    }
+    const hide_delete = () => {
+        let element = document.getElementById("my-delete");
+        element.classList.remove("my-style");
 
-
-
+        let element_back = document.getElementById("open-files-background-delete");
+        element_back.classList.remove("see-update");
+    }
 
 
     return (
@@ -77,8 +81,6 @@ const Admin_profile = () => {
                                     </div>
                                 </div>
                             </div>
-
-
                             <div>
                                 <div className='admin-p-profile-buttons-down'>
 
@@ -86,8 +88,8 @@ const Admin_profile = () => {
                                         Actualizar
 
                                     </div>
-                                    <div className='admin-p-profile-buttons-down-delete'>
-                                        Eliminar Usuario
+                                    <div  onClick={() => see_delete()} className='admin-p-profile-buttons-down-delete'>
+                                        Borrar cuenta
                                     </div>
                                 </div>
                             </div>
@@ -214,30 +216,64 @@ const Admin_profile = () => {
                         </div>
                     </div>
 
-
                     <div className="" id="open-files-background">
                         <div className="class-update" id="my-update">
-                            
+
                             <div className="close-window" id="X" onClick={() => hide_update()}>X</div>
 
                             <div className='iframe-arquitects-pop-up'>
-                            <input  className='iframe-arquitects-pop-up-data' type="text" name="name" title="name" lenght="30" placeholder="Nombre"></input>
+                                <input className='iframe-arquitects-pop-up-data' type="text" name="name" title="name" lenght="30" placeholder="Nombre"></input>
                             </div>
                             <div className='iframe-arquitects-pop-up'>
-                            <input  className='iframe-arquitects-pop-up-data' type="text" name="name" title="name" lenght="30" placeholder="Apellidos"></input>
+                                <input className='iframe-arquitects-pop-up-data' type="text" name="name" title="name" lenght="30" placeholder="Apellidos"></input>
                             </div>
                             <div className='iframe-arquitects-pop-up'>
-                            <input  className='iframe-arquitects-pop-up-data' type="text" name="name" title="name" lenght="30" placeholder="Teléfono"></input>
+                                <input className='iframe-arquitects-pop-up-data' type="text" name="name" title="name" lenght="30" placeholder="Teléfono"></input>
                             </div>
                             <div>
                                 <p className='update-email-nochange'>*El email no se puede cambiar</p>
                             </div>
                             <div className="update-send-data" /*onClick={() => send_data_backend()}*/>Actualizar</div>
-                            
 
-                           
                         </div>
+
+                        <div className="" id="open-files-background-delete">
+                            <div className="class-update" id="my-delete">
+
+                                <div className="close-window" id="X" onClick={() => hide_delete()}>X</div>
+                                <h3>¿Quieres borrar tu cuenta?</h3>
+                                <h4>Perderás todos los datos actuales</h4>
+                                <div className='update-delete-user'>
+                                    <div onClick={() => hide_delete()} className='update-delete-user-buttons no-delete'>
+                                        No
+                                    </div>
+                                    <div className='update-delete-user-buttons yes-delete'>
+                                        Sí
+                                    </div>
+
+                                </div>
+
+                                <div className='iframe-arquitects-pop-up'>
+
+
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
                     </div>
+
 
 
 
